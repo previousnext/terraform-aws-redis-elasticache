b6da478dfbb75ea48f3ad40a2ef2771f232bac52
Nick Santamaria <nick@nicksantamaria.net>
2018-04-12 22:58:08 +1000

    Commented out problematic output.

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,6 @@ output "port" {
   value = "6379"
 }
 
-output "endpoint" {
-  value = "${aws_elasticache_replication_group.redis.primary_endpoint_address}"
-}
+#output "endpoint" {
+#  value = "${aws_elasticache_replication_group.redis.primary_endpoint_address}"
+#}
